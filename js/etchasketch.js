b40397022179f38392908125ffedcfdcaf1699f8
Andy <andeh79@gmail.com>
2017-05-22 23:55:00 -0400

    Add hover color change functionality

--- js/etchasketch.js
+++ js/etchasketch.js
@@ -1,18 +1,21 @@
 $(document).ready(function(){
-	
-	$("#gridContainer").hide();
-	$("#gridContainer").fadeIn("slow");
-	addGridBlocks();
+
+  $("#gridContainer").hide();
+  $("#gridContainer").fadeIn("slow");
+  addGridBlocks();
+  
+  $(".gridBlock").hover(function(){
+    $(this).css({"background": "black"});
+  });
 });
 
 //Generates all grid elements to a 16x16 grid.
 function addGridBlocks(){
-	var block = '<div class="gridBlock"></div>';
+  var block = '<div class="gridBlock">';
+  var gridSize = 256;
+  
+  for(var i = 1; i <= gridSize; i++){
+    $("#gridContainer").append(block);
 
-	for(var i = 1; i <= 256; i++){
-		$("#gridContainer").append(block);
-    if(i % 16 === 0){
-          $("#gridContainer").append("<br>");
-    }
-	}
+  }
 }
