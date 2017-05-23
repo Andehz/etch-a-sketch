db788442d5da2c6c98604a6ce1d022d739f8ffeb
Andy <andeh79@gmail.com>
2017-05-23 16:43:01 -0400

    Add reset logic and formatting logic. Add color creation logic by setting it via random RGB

--- js/etchasketch.js
+++ js/etchasketch.js
@@ -1,21 +1,56 @@
 $(document).ready(function(){
-
+  var gridSize = 16;
+  
   $("#gridContainer").hide();
   $("#gridContainer").fadeIn("slow");
-  addGridBlocks();
+  addGridBlocks(gridSize);
   
+  //Reset grid on button click
+  $("button").click(function(){
+    gridSize = prompt("Grid size?")
+    $("#gridContainer").empty();
+    addGridBlocks(gridSize);
+    $(".gridBlock").hover(function(){
+      $(this).css({"background": setRGB()});
+    });
+  });
+  
+  //Changes color when arrow is over it
   $(".gridBlock").hover(function(){
-    $(this).css({"background": "black"});
+    $(this).css({"background": setRGB()});
   });
 });
 
-//Generates all grid elements to a 16x16 grid.
-function addGridBlocks(){
+function setRGB(){
+  var valueR = Math.floor(Math.random() * 256);
+  var valueG = Math.floor(Math.random() * 256);
+
+  var valueB = Math.floor(Math.random() * 256);
+  return "rgb(" + valueR + ", " + valueG + ", " + valueB +")";
+}
+//Generates all grid elements to a 16x16 grid to start.
+function addGridBlocks(totalBlocks){
   var block = '<div class="gridBlock">';
-  var gridSize = 256;
   
-  for(var i = 1; i <= gridSize; i++){
+  for(var i = 0; i < getTotalBlocks(totalBlocks); i++){
     $("#gridContainer").append(block);
 
   }
+  
+  setBlockSize(totalBlocks);
+}
+
+//sets size of each block in em.
+function setBlockSize(size){
+  //35 is default size of whole grid.
+  var blockSize = 35/size + "em";
+  
+  $(".gridBlock").width(blockSize);
+  $(".gridBlock").height(blockSize);
+}
+
+function getTotalBlocks(gridSize){
+  var totalBlocks = gridSize * gridSize;
+
+  return totalBlocks;
 }
